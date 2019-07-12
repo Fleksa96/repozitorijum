90f089d8bf4313cb0e3989aeef96654b9b22f7c6
Aleksa <aleksapetrovicks@gmail.com>
2019-07-12 12:14:02 +0200

    asd

--- config/production.py
+++ config/production.py
@@ -4,12 +4,13 @@ class Production(Config):
 
     ENV_TYPE = "Production" 
 
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
-    DB_NAME = None
-    DB_USER = None
-    DB_PASSWORD = None
-    DB_HOST = None
-    DB_PORT = None
+    DB_NAME = "d2ahcqfavaeia4"
+    DB_USER = "ntleqhjmibyqmo"
+    DB_PASSWORD = "b3b483d90db37045bca3a86b194233c9098100f72d78364692c77f165306a581"
+    DB_HOST = "ec2-107-21-120-104.compute-1.amazonaws.com"
+    DB_PORT = 5432
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
 
     username = "Aleksa"
     password = "123456"
